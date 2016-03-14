12f84e5593e4488b1362013628f959fc4374c9aa
Laurent Callarec <l.callarec@gmail.com>
2016-03-14 14:10:25 +0100

    Review pre-preocess script

--- build/pre-process.py
+++ build/pre-process.py
@@ -5,39 +5,74 @@ import shutil
 import sys
 
 
-def copy_source_files(from_path, to_path):
+class PreProcessor:
+	
+	def __init__(self, from_path, to_path):
+		self.from_path = from_path
+		self.to_path   = to_path
+	
+	def copy(self):
+	
+		for root, filename in self.__iterate_files(self.from_path):
+			if filename.endswith('.vala'):
+				basename = (root + os.path.sep + filename).replace(os.path.sep, '_')
+				shutil.copy(os.path.join(root, filename), self.to_path + os.path.sep + basename)
 
-    for root, directories, filenames in os.walk(from_path):
-        for filename in filenames:
-            if filename.endswith('.vala'):
-                basename = (root + os.path.sep + filename).replace(os.path.sep, '_')
-                shutil.copy(os.path.join(root, filename), to_path + os.path.sep + basename)
+	def pre_process_for(self, flag):
+		processed_content = ""
+		for root, filename in self.__iterate_files(self.to_path):
+			print root + os.path.sep + filename
+			with open(root + os.path.sep + filename, 'r') as file:
+				must_be_removed = False
+				for line in file:
+					if "#if " + flag in line:
+						must_be_removed = True
+					elif must_be_removed == True and "#endif" not in line:
+						continue
+					elif must_be_removed == True and "#endif" in line:
+						must_be_removed = False
+						continue
+					else:
+						processed_content += line
 
-def pre_process_for(source_path, flag):
+				file.close()
 
-    processed_content = ""
-    for root, directories, filenames in os.walk(source_path):
-        for filename in filenames:
-            with open(root + os.path.sep + filename, 'r') as file:
-                must_be_removed = False
-                for line in file:
-                    if "#if " + flag in line:
-                        must_be_removed = True
-                    elif must_be_removed == True and "#endif" not in line:
-                        continue
-                    elif must_be_removed == True and "#endif" in line:
-                        must_be_removed = False
-                        continue
-                    else:
-                        processed_content += line
+			with open(root + os.path.sep + filename, 'w') as file:
+				file.write(processed_content)
+				file.close()
 
-                file.close()
+			processed_content = ""
 
-            with open(root + os.path.sep + filename, 'w') as file:
-                file.write(processed_content)
-                file.close()
+	def clean_pre_process(self, flag):
 
-            processed_content = ""
+		processed_content = ""
+		for root, filename in self.__iterate_files(self.to_path):
+			with open(root + os.path.sep + filename, 'r') as file:
+				must_be_removed = False
+				for line in file:
+					if "#if " + flag in line:
+						must_be_removed = True
+					elif must_be_removed == True and "#endif" not in line:
+						processed_content += line
+					elif must_be_removed == True and "#endif" in line:
+						must_be_removed = False
+						continue
+					else:
+						processed_content += line
+
+				file.close()
+
+			with open(root + os.path.sep + filename, 'w') as file:
+				file.write(processed_content)
+				file.close()
+
+			processed_content = ""
+				
+	def __iterate_files(self, path):
+		for root, directories, filenames in os.walk(path):
+			for filename in filenames:
+				yield root, filename			
+
 
 class GtkVersions:
 
@@ -74,19 +109,24 @@ class GtkVersions:
             versions = self.get_versions()
             version_is_at = versions.index(str(version))
 
-            return versions[version_is_at:]
+            return versions[version_is_at + 1:]
 
         except ValueError:
             return None
 
 
 if __name__ == '__main__':
-    copy_source_files(sys.argv[1], sys.argv[2])
+	pre_processor = PreProcessor(sys.argv[1], sys.argv[2])
+	pre_processor.copy()
 
     #GTK pre-processor
-    gtk_versions = GtkVersions()
-    for version in gtk_versions.get_versions_prior(sys.argv[3]):
-        pre_process_for(sys.argv[2], "GTK_LTE_" + version.replace(".", "_"))
+	gtk_versions = GtkVersions()
+	for version in gtk_versions.get_versions_prior(sys.argv[3]):
+		pre_processor.pre_process_for("GTK_LTE_" + version.replace(".", "_"))
 
-    for version in gtk_versions.get_versions_after(sys.argv[3]):
-        pre_process_for(sys.argv[2], "GTK_GTE_" + version.replace(".", "_"))
+	for version in gtk_versions.get_versions_after(sys.argv[3]):
+		pre_processor.pre_process_for("GTK_GTE_" + version.replace(".", "_"))
+        
+	for version in gtk_versions.get_versions_after(sys.argv[3]):
+		pre_processor.clean_pre_process("GTK_")    
+        
